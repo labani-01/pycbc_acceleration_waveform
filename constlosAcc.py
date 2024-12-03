6f7fa1dec5cc406c47bf874b2a04cde7d4f5fb68
labani-01 <labanoy2000@gmail.com>
2024-12-03 12:16:16 -0500

    considering_|at|<1_condition

--- constlosAcc.py
+++ constlosAcc.py
@@ -6,15 +6,18 @@ def const_los_Acc_td(v0, acc, base_model, **kwds):
     import numpy as np
     if 'approximant' in kwds:
         kwds.pop('approximant')
-    hp,hc = get_td_waveform(approximant = base_model, **kwds)
-    t1 = hp.sample_times[np.argmax(hp)+1:-1] #after zero
-    t2 = hp.sample_times[1:np.argmax(hp)+2]
+    hp_base,hc_base = get_td_waveform(approximant = base_model, **kwds)
+    filtered_array = hp_base.sample_times[np.absolute(acc * hp_base.sample_times)<1]
+    hp = hp_base[np.absolute(acc * hp_base.sample_times)<1]
+    hc = hc_base[np.absolute(acc * hp_base.sample_times)<1]
+    t1 = filtered_array[np.argmax(hp)+1:-1] #after zero
+    t2 = filtered_array[1:np.argmax(hp)+2]
     dt1 = np.full((len(t1),), dt, dtype=float)
     dt2 = np.full((len(t2),), dt, dtype=float)
     dt1_new = (1 + v0 + acc * t1)* dt1
-    dt1_new = np.insert(dt1_new, 0, hp.sample_times[np.argmax(hp)+1])
+    dt1_new = np.insert(dt1_new, 0, filtered_array[np.argmax(hp)+1])
     dt2_new = -(1 + v0 + acc * t2)* dt2
-    dt2_new = np.insert(dt2_new, np.argmax(hp)+1, hp.sample_times[np.argmax(hp)+1])
+    dt2_new = np.insert(dt2_new, np.argmax(hp)+1, filtered_array[np.argmax(hp)+1])
     t1_new = np.cumsum(dt1_new)
     t2_new = np.cumsum(dt2_new[::-1])[::-1]
     t2_new = t2_new[:-1]
